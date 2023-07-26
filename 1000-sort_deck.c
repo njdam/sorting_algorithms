9dbdaba45107ead904ea2d4a39df71f84355b5e8
njdam <jeandamngaboyimana@gmail.com>
2023-07-26 04:16:50 +0000

    Updated.

---
+++ 1000-sort_deck.c
@@ -0,0 +1,15 @@
+#include "deck.h"
+
+/**
+ * sort_deck - is func to sort a deck of cards;
+ * @deck: is a head node of a deck_node_t doubly-linked list;
+ *
+ * Return: nothing.
+ */
+void sort_deck(deck_node_t **deck)
+{
+	if (!deck || !(*deck) || !((*deck)->next))
+		return;
+	else
+		return;
+}
